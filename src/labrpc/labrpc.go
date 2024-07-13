ff6bc97e55240c9364cdd14a673c40534ec0438d
mwfj <mwfj0215@gmail.com>
2024-07-13 17:22:56 +0800

    add DeleteEnd func

--- src/labrpc/labrpc.go
+++ src/labrpc/labrpc.go
@@ -49,15 +49,18 @@ package labrpc
 //   pass svc to srv.AddService()
 //
 
-import "6.5840/labgob"
-import "bytes"
-import "reflect"
-import "sync"
-import "log"
-import "strings"
-import "math/rand"
-import "time"
-import "sync/atomic"
+import (
+	"bytes"
+	"log"
+	"math/rand"
+	"reflect"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"6.5840/labgob"
+)
 
 type reqMsg struct {
 	endname  interface{} // name of sending ClientEnd
@@ -327,6 +330,18 @@ func (rn *Network) MakeEnd(endname interface{}) *ClientEnd {
 	rn.connections[endname] = nil
 
 	return e
+}
+
+func (rn *Network) DeleteEnd(endname interface{}) {
+	rn.mu.Lock()
+	defer rn.mu.Unlock()
+
+	if _, ok := rn.ends[endname]; !ok {
+		log.Fatalf("MakeEnd: %v doesn't exists\n", endname)
+	}
+	delete(rn.ends, endname)
+	delete(rn.enabled, endname)
+	delete(rn.connections, endname)
 }
 
 func (rn *Network) AddServer(servername interface{}, rs *Server) {
